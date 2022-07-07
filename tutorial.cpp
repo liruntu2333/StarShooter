28aca29f82485595aeb6a905ab12cc1f0e512257
黄源 THS21 <ths10752@ths.hal.ac.jp>
2022-07-07 17:46:31 +0900

    チュートリアル画面のcppとｈを追加する

---
+++ tutorial.cpp
@@ -0,0 +1,176 @@
+//=============================================================================
+//
+// チュートリアル画面処理 [tutorial.cpp]
+// Author : 
+//
+//=============================================================================
+#include "main.h"
+#include "renderer.h"
+#include "input.h"
+#include "fade.h"
+//#include "sound.h"
+#include "sprite.h"
+#include "tutorial.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define TEXTURE_WIDTH				(SCREEN_WIDTH)	// 背景サイズ
+#define TEXTURE_HEIGHT				(SCREEN_HEIGHT)	// 
+#define TEXTURE_MAX					(1)				// テクスチャの数
+
+#define TEXTURE_WIDTH_LOGO			(480)			// ロゴサイズ
+#define TEXTURE_HEIGHT_LOGO			(80)			// 
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+static ID3D11Buffer* g_VertexBuffer = NULL;					// 頂点情報
+static ID3D11ShaderResourceView* g_Texture[TEXTURE_MAX] = { NULL };		// テクスチャ情報
+
+static char* g_TexturName[TEXTURE_MAX] = {
+	"data/TEXTURE/TUTORIAL/tutorial_bg_test.png",
+};
+
+
+static TUTORIAL	g_Tutorial;				// タイトル画面のbackground
+
+static BOOL		g_Load = FALSE;
+
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT InitTutorial(void)
+{
+	ID3D11Device* pDevice = GetDevice();
+
+	//テクスチャ生成
+	for (int i = 0; i < TEXTURE_MAX; i++)
+	{
+		g_Texture[i] = NULL;
+		D3DX11CreateShaderResourceViewFromFile(GetDevice(),
+			g_TexturName[i],
+			NULL,
+			NULL,
+			&g_Texture[i],
+			NULL);
+	}
+
+
+	// 頂点バッファ生成
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.Usage = D3D11_USAGE_DYNAMIC;
+	bd.ByteWidth = sizeof(VERTEX_3D) * 4;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	GetDevice()->CreateBuffer(&bd, NULL, &g_VertexBuffer);
+
+
+	// 変数の初期化
+	// タイトル画面のbackground
+	g_Tutorial.use = TRUE;
+	g_Tutorial.widtht = TEXTURE_WIDTH;
+	g_Tutorial.height = TEXTURE_HEIGHT;
+	g_Tutorial.pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_Tutorial.texNo = TEXTURE_TUTORIAL_BG;
+
+	// タイトル画面のlogo
+
+
+	// BGM再生
+	//PlaySound(SOUND_LABEL_BGM_sample000);
+
+	g_Load = TRUE;
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void UninitTutorial(void)
+{
+	if (g_Load == FALSE) return;
+
+	if (g_VertexBuffer)
+	{
+		g_VertexBuffer->Release();
+		g_VertexBuffer = NULL;
+	}
+
+	for (int i = 0; i < TEXTURE_MAX; i++)
+	{
+		if (g_Texture[i])
+		{
+			g_Texture[i]->Release();
+			g_Texture[i] = NULL;
+		}
+	}
+
+	g_Load = FALSE;
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void UpdateTutorial(void)
+{
+
+	if (GetKeyboardTrigger(DIK_RETURN))
+	{// Enter押したら、ステージを切り替える
+		SetFade(FADE_OUT, MODE_GAME);
+	}
+
+
+#ifdef _DEBUG	// デバッグ情報を表示する
+
+
+#endif
+
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void DrawTutorial(void)
+{
+	// 頂点バッファ設定
+	UINT stride = sizeof(VERTEX_3D);
+	UINT offset = 0;
+	GetDeviceContext()->IASetVertexBuffers(0, 1, &g_VertexBuffer, &stride, &offset);
+
+	// マトリクス設定
+	SetWorldViewProjection2D();
+
+	// プリミティブトポロジ設定
+	GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+
+	// マテリアル設定
+	MATERIAL material;
+	ZeroMemory(&material, sizeof(material));
+	material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	SetMaterial(material);
+
+	// タイトルの背景を描画
+	{
+		// テクスチャ設定
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[g_Tutorial.texNo]);
+
+		// １枚のポリゴンの頂点とテクスチャ座標を設定
+		SetSpriteLeftTop(g_VertexBuffer, g_Tutorial.pos.x, g_Tutorial.pos.y, g_Tutorial.widtht, g_Tutorial.height, 0.0f, 0.0f, 1.0f, 1.0f);
+
+		// ポリゴン描画
+		GetDeviceContext()->Draw(4, 0);
+	}
+}
+
+
+
+
+

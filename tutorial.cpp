aacd5c17ae0e867b2720327aca95063fd2cfd820
黄源 <hal.kougen.2021@gmail.com>
2022-08-31 21:54:26 +0900

    チュートリアル画面を追加

--- tutorial.cpp
+++ tutorial.cpp
@@ -17,10 +17,15 @@
 //*****************************************************************************
 #define TEXTURE_WIDTH				(SCREEN_WIDTH)	// 背景サイズ
 #define TEXTURE_HEIGHT				(SCREEN_HEIGHT)	// 
-#define TEXTURE_MAX					(1)				// テクスチャの数
 
-#define TEXTURE_WIDTH_LOGO			(480)			// ロゴサイズ
-#define TEXTURE_HEIGHT_LOGO			(80)			// 
+#define TEXTURE_WIDTH_TIPS			(500.0f)	// 背景サイズ
+#define TEXTURE_HEIGHT_TIPS			(200.0f)	// 
+
+#define TEXTURE_TUTORIAL_MAX		(4)
+#define TEXTURE_TIPS_MAX			(2)
+
+#define TEXTURE_MAX					(TEXTURE_TUTORIAL_MAX + TEXTURE_TIPS_MAX)				// テクスチャの数
+
 
 //*****************************************************************************
 // プロトタイプ宣言
@@ -34,11 +39,18 @@ static ID3D11Buffer* g_VertexBuffer = NULL;					// 頂点情報
 static ID3D11ShaderResourceView* g_Texture[TEXTURE_MAX] = { NULL };		// テクスチャ情報
 
 static char* g_TexturName[TEXTURE_MAX] = {
-	"data/TEXTURE/TUTORIAL/tutorial_bg_test.png",
+	"data/TEXTURE/TUTORIAL/tutorial1.png",
+	"data/TEXTURE/TUTORIAL/tutorial2.png",
+	"data/TEXTURE/TUTORIAL/tutorial3.png",
+	"data/TEXTURE/TUTORIAL/tutorial4.png",
+	"data/TEXTURE/TUTORIAL/tips.png",
+	"data/TEXTURE/TUTORIAL/tips2.png",
 };
 
 
-static TUTORIAL	g_Tutorial;				// タイトル画面のbackground
+static TUTORIAL	g_Tutorial[TEXTURE_TUTORIAL_MAX];
+
+static TUTORIAL g_Tips;
 
 static BOOL		g_Load = FALSE;
 
@@ -74,14 +86,41 @@ HRESULT InitTutorial(void)
 
 
 	// 変数の初期化
-	// タイトル画面のbackground
-	g_Tutorial.use = TRUE;
-	g_Tutorial.widtht = TEXTURE_WIDTH;
-	g_Tutorial.height = TEXTURE_HEIGHT;
-	g_Tutorial.pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	g_Tutorial.texNo = TEXTURE_TUTORIAL_BG;
+	g_Tutorial[0].use = TRUE;
+	g_Tutorial[0].alpha = 1.0f;
+	g_Tutorial[0].widtht = TEXTURE_WIDTH;
+	g_Tutorial[0].height = TEXTURE_HEIGHT;
+	g_Tutorial[0].pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_Tutorial[0].texNo = TEXTURE_TUTORIAL_1;
 
-	// タイトル画面のlogo
+	g_Tutorial[1].use = FALSE;
+	g_Tutorial[1].alpha = 0.0f;
+	g_Tutorial[1].widtht = TEXTURE_WIDTH;
+	g_Tutorial[1].height = TEXTURE_HEIGHT;
+	g_Tutorial[1].pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_Tutorial[1].texNo = TEXTURE_TUTORIAL_2;
+
+	g_Tutorial[2].use = FALSE;
+	g_Tutorial[2].alpha = 0.0f;
+	g_Tutorial[2].widtht = TEXTURE_WIDTH;
+	g_Tutorial[2].height = TEXTURE_HEIGHT;
+	g_Tutorial[2].pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_Tutorial[2].texNo = TEXTURE_TUTORIAL_3;
+
+	g_Tutorial[3].use = FALSE;
+	g_Tutorial[3].alpha = 0.0f;
+	g_Tutorial[3].widtht = TEXTURE_WIDTH;
+	g_Tutorial[3].height = TEXTURE_HEIGHT;
+	g_Tutorial[3].pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_Tutorial[3].texNo = TEXTURE_TUTORIAL_4;
+
+	g_Tips.use = TRUE;
+	g_Tips.alpha = 1.0f;
+	g_Tips.widtht = TEXTURE_WIDTH_TIPS;
+	g_Tips.height = TEXTURE_HEIGHT_TIPS;
+	g_Tips.pos = XMFLOAT3(SCREEN_WIDTH - TEXTURE_WIDTH_TIPS, SCREEN_HEIGHT - TEXTURE_HEIGHT_TIPS, 0.0f);
+	g_Tips.texNo = TEXTURE_TUTORIAL_TIPS_1;
+	
 
 
 	// BGM再生
@@ -127,6 +166,67 @@ void UpdateTutorial(void)
 		SetFade(FADE_OUT, MODE_GAME);
 	}
 
+	if (GetKeyboardTrigger(DIK_RIGHT))
+	{
+		for (int i = 0; i < TEXTURE_TUTORIAL_MAX; i++)
+		{
+			if (g_Tutorial[i].use == TRUE)
+			{
+				if (i == 3)
+				{
+					continue;
+				}
+				else
+				{
+					g_Tutorial[i].use = FALSE;
+					g_Tutorial[i + 1].use = TRUE;
+					break;
+				}
+				
+			}
+		}
+	}
+
+	if (g_Tutorial[3].use == TRUE)
+	{
+		g_Tips.texNo = TEXTURE_TUTORIAL_TIPS_2;
+	}
+
+
+	for (int i = 0; i < TEXTURE_TUTORIAL_MAX; i++)
+	{
+		if (g_Tutorial[i].use == TRUE)
+		{		
+			g_Tutorial[i].alpha = 1.0f;
+
+		}
+		else
+		{
+			g_Tutorial[i].alpha = 0.0f;
+		}
+	}
+
+
+
+
+	if (g_Tips.use == TRUE)
+	{
+		g_Tips.alpha -= 0.02f;
+		if (g_Tips.alpha <= 0.0f)
+		{
+			g_Tips.alpha = 0.0f;
+			g_Tips.use = FALSE;
+		}
+	}
+	else
+	{
+		g_Tips.alpha += 0.02f;
+		if (g_Tips.alpha >= 1.0f)
+		{
+			g_Tips.alpha = 1.0f;
+			g_Tips.use = TRUE;
+		}
+	}
 
 #ifdef _DEBUG	// デバッグ情報を表示する
 
@@ -157,13 +257,31 @@ void DrawTutorial(void)
 	material.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 	SetMaterial(material);
 
-	// タイトルの背景を描画
+	// チュートリアルを描画
+	{
+		for (int i = 0; i < TEXTURE_TUTORIAL_MAX; i++)
+		{
+			// テクスチャ設定
+			GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[g_Tutorial[i].texNo]);
+
+			// １枚のポリゴンの頂点とテクスチャ座標を設定
+			SetSpriteColor_LeftTop(g_VertexBuffer, g_Tutorial[i].pos.x, g_Tutorial[i].pos.y, g_Tutorial[i].widtht, g_Tutorial[i].height, 0.0f, 0.0f, 1.0f, 1.0f, XMFLOAT4(1.0f, 1.0f, 1.0f, g_Tutorial[i].alpha));
+
+			
+			// ポリゴン描画
+			GetDeviceContext()->Draw(4, 0);
+		}
+		
+	}
+
+	// Tipsを描画
 	{
 		// テクスチャ設定
-		GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[g_Tutorial.texNo]);
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_Texture[g_Tips.texNo]);
 
 		// １枚のポリゴンの頂点とテクスチャ座標を設定
-		SetSpriteLeftTop(g_VertexBuffer, g_Tutorial.pos.x, g_Tutorial.pos.y, g_Tutorial.widtht, g_Tutorial.height, 0.0f, 0.0f, 1.0f, 1.0f);
+		SetSpriteColor_LeftTop(g_VertexBuffer, g_Tips.pos.x, g_Tips.pos.y, g_Tips.widtht, g_Tips.height, 0.0f, 0.0f, 1.0f, 1.0f, XMFLOAT4(1.0f, 1.0f, 1.0f, g_Tips.alpha));
+
 
 		// ポリゴン描画
 		GetDeviceContext()->Draw(4, 0);
